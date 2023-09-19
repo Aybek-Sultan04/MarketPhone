994c417cb69e48064be62f89cc6908c6d5b8f5ab
Aybek-Sultan04 <sultanov.aybek18@gmail.com>
2023-09-19 18:31:00 +0300

    add new function

--- MarketPhone.UI/Properties/Resources.Designer.cs
+++ MarketPhone.UI/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace MarketPhone.UI.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap SingInImages {
+            get {
+                object obj = ResourceManager.GetObject("SingInImages", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
