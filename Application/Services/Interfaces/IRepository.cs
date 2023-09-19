9dd269fa3c6af8d8918788c0e69cd7e300438366
Laziz <hamroyevlaziz007@gmail.com>
2023-09-19 16:27:45 +0500

    Add Application service and Packages installed

---
+++ Application/Services/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Interfaces
+{
+   public class IRepository<T>
+    {
+        int Create(T entity);
+        int Update(T entity);
+        int Delete(T entity);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+    }
+}
